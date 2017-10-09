83e8dece72af0007df02c7a3b624e5ae14219595
Ian Pansensoy <ian.pansensoy@news.com.au>
2017-10-09 15:35:37 +0800

    api facebook

--- server.js
+++ server.js
@@ -5,11 +5,26 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const authController = require('./controllers/authController');
 const userController = require('./controllers/usersController');
+const facebookController = require('./controllers/facebookController');
 const configurePassport = require('./config/passport-jwt-config');
 const apiPrefix = "/api";
 const morgan = require('morgan');
-
+const facebookConfig = require('./config/social-config');
+const FacebookStrategy = require('passport-facebook').Strategy;
 const app = express();
+
+// passport.use(new FacebookStrategy({
+//     clientID: facebookConfig.facebook.appId,
+//     clientSecret: facebookConfig.facebook.appSecret,
+//     callbackURL: "http://localhost:3000/api/auth/facebook/callback"
+//   },
+//   function(accessToken, refreshToken, profile, done) {
+
+//   }
+// ));
+
+
+
 app.set("port", process.env.PORT || 3001);
 
 app.use(passport.initialize());
@@ -20,6 +35,9 @@ app.use(morgan('dev'));
 
 app.use(apiPrefix + '/auth', authController);
 app.use(apiPrefix + '/users', userController);
+app.use(apiPrefix + '/auth-redirect',facebookController);
+
+
 
 app.listen(app.get("port"), () => {
   console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
